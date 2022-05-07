a31307f2e5ec8c342ac5749b9d8283cdd2d34ee9
biiwz <biiwzmethom@gmail.com>
2022-05-07 14:34:24 +0700

    axios basic

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
+import axios from 'axios';
+
 const AboutPage = () => {
+    const [version, setVersion] = React.useState('');
+    const getData = async () => {
+        const resp = await axios.get('https://api.codingthailand.com/api/version');
+        console.log(resp.data.data.version);
+        setVersion(resp.data.data.version);
+    }
+
+    React.useEffect(() => {
+        getData();
+    }, []);
     return (
         <div className='container'>
             <div className='row'>
                 <div className='col-12 mt-4'>
                     <h1>About</h1>
+                    {
+                        version && (
+                            <p>Backend api version: {version}</p>
+                        )
+                    }
                 </div>
             </div>
         </div>
